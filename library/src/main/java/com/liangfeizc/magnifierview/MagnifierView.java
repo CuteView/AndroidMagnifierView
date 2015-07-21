32305a230b7a3dcbed8e3a2147ac7a850a3b54ca
liangfei <njliangfei@gmail.com>
2015-07-22 01:06:30 +0800

    create project

---
+++ library/src/main/java/com/liangfeizc/magnifierview/MagnifierView.java
@@ -0,0 +1,29 @@
+package com.liangfeizc.magnifierview;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by liangfeizc on 7/22/15.
+ */
+public class MagnifierView extends View {
+    public MagnifierView(Context context) {
+        super(context);
+        init();
+    }
+
+    public MagnifierView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public MagnifierView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    private void init() {
+
+    }
+}
