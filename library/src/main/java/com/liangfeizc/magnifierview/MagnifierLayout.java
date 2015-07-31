d9a3d01e461f798c8e9dcc1276141b281bb1bc74
liangfei <njliangfei@gmail.com>
2015-07-31 16:42:37 +0800

    update code

--- library/src/main/java/com/liangfeizc/magnifierview/MagnifierLayout.java
+++ library/src/main/java/com/liangfeizc/magnifierview/MagnifierLayout.java
@@ -1,6 +1,7 @@
 package com.liangfeizc.magnifierview;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.support.annotation.NonNull;
 import android.util.AttributeSet;
 import android.util.Log;
@@ -16,6 +17,7 @@ public class MagnifierLayout extends FrameLayout {
     private static final String TAG = "MagnifierLayout";
 
     private PopupWindow mPopupWindow;
+    private MagnifierView mMagnifierView;
 
     public MagnifierLayout(Context context) {
         super(context);
@@ -33,23 +35,27 @@ public class MagnifierLayout extends FrameLayout {
     }
 
     private void init() {
+        mMagnifierView = new MagnifierView(getContext());
+        mMagnifierView.setBackgroundColor(Color.BLACK);
+
         mPopupWindow = new PopupWindow(getContext());
+        mPopupWindow.setContentView(mMagnifierView);
         mPopupWindow.setWidth(100);
         mPopupWindow.setHeight(100);
     }
 
     private void show(float x, float y) {
+        mPopupWindow.dismiss();
         mPopupWindow.showAtLocation(this, Gravity.BOTTOM, (int) x, (int) y);
     }
 
     @Override
     public boolean onTouchEvent(@NonNull MotionEvent event) {
-        Log.d(TAG, "onTouchVent " + event.getX() + ", " + event.getY());
-
         switch (event.getAction()) {
             case MotionEvent.ACTION_DOWN:
                 return true;
             case MotionEvent.ACTION_MOVE:
+                Log.d(TAG, "onTouchVent " + event.getX() + ", " + event.getY());
                 show(event.getX(), event.getY());
                 return true;
         }
