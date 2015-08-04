c1370a19fcbc76fbb6aa7b717778849ae8b5531c
liangfei <njliangfei@gmail.com>
2015-08-05 01:06:21 +0800

    move pop while touching move

--- library/src/main/java/com/liangfeizc/magnifierview/MagnifierLayout.java
+++ library/src/main/java/com/liangfeizc/magnifierview/MagnifierLayout.java
@@ -2,11 +2,13 @@ package com.liangfeizc.magnifierview;
 
 import android.content.Context;
 import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.support.annotation.NonNull;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.MotionEvent;
+import android.view.ViewGroup;
 import android.widget.FrameLayout;
 import android.widget.PopupWindow;
 
@@ -36,27 +38,32 @@ public class MagnifierLayout extends FrameLayout {
 
     private void init() {
         mMagnifierView = new MagnifierView(getContext());
-        mMagnifierView.setBackgroundColor(Color.BLACK);
 
-        mPopupWindow = new PopupWindow(getContext());
+        mPopupWindow = new PopupWindow(mMagnifierView,
+                ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
         mPopupWindow.setContentView(mMagnifierView);
+        mPopupWindow.setBackgroundDrawable(new ColorDrawable(Color.RED));
         mPopupWindow.setWidth(100);
         mPopupWindow.setHeight(100);
     }
 
     private void show(float x, float y) {
-        mPopupWindow.dismiss();
-        mPopupWindow.showAtLocation(this, Gravity.BOTTOM, (int) x, (int) y);
+        mPopupWindow.showAtLocation(this, Gravity.NO_GRAVITY, (int) x, (int) y);
     }
 
     @Override
     public boolean onTouchEvent(@NonNull MotionEvent event) {
         switch (event.getAction()) {
             case MotionEvent.ACTION_DOWN:
+                Log.d(TAG, "onTouchVent " + event.getRawX() + ", " + event.getRawY());
+                if (mPopupWindow.isShowing()) {
+                    mPopupWindow.dismiss();
+                }
+                show(event.getRawX(), event.getRawY());
                 return true;
             case MotionEvent.ACTION_MOVE:
-                Log.d(TAG, "onTouchVent " + event.getX() + ", " + event.getY());
-                show(event.getX(), event.getY());
+                mPopupWindow.update((int) event.getRawX(), (int) event.getRawY(), -1, -1);
+                //show(event.getX(), event.getY());
                 return true;
         }
         return super.onTouchEvent(event);
